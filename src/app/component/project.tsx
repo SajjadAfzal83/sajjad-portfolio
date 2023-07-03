a40e9699cc6aa87fb3a56da22c3eb097d45dbf72
SajjadAfzal83 <soafzal@gmail.com>
2023-07-04 03:06:02 +0500

    Update project.tsx

--- src/app/component/project.tsx
+++ src/app/component/project.tsx
@@ -15,13 +15,13 @@ const Project = () => {
     <div className="flex flex-wrap -m-5 -mt-[5rem]">
       <div className="lg:w-1/3 p-4">
         <div className="flex relative rounded-md">
-          <Image
+          {/* <Image
             alt="gallery"
             className="absolute inset-0 w-full h-full object-cover object-center"
             src={project}
             width={500}
             height={500}
-          />
+          /> */}
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
             <h2 className="tracking-widest text-sm title-font font-medium text-red-900 mb-1">
               My Portfolio
